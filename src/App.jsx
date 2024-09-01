52daa1367d048cb730873f9f0461adfb7f3c8ef5
DEVANSH ARYAN <135739248+devansharyan123@users.noreply.github.com>
2024-09-01 22:09:01 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
    <div className="relative px-8 py-3 font-bold text-white bg-transparent border-2 border-transparent rounded-md transition-all duration-300 
     border-opacity-0 hover:border-opacity-100 hover:border-transparent hover:shadow-[0_0_20px_10px_rgba(192,192,192,0.7)] hover:ring-2 
     hover:ring-transparent hover:ring-offset-4 hover:ring-offset-transparent">
-      <h1 className='font-bold text-white'>Add Your Todo List</h1>
+      <h1 className='font-bold text-black'>Add Your Todo List</h1>
       <AddTodo />
       <Todos  />
       </div>
